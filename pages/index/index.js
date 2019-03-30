a4d9653b0a18bc471715b9b236fa251ed5e288cd
yanzizhi <1120161668@qq.com>
2019-03-30 22:03:16 +0800

    封装URL

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,17 +1,48 @@
 //index.js
 //获取应用实例
 const app = getApp();
-let getMusic = require('../../utils/config.js');
+let getMusicUrl = require('../../utils/config.js');
 
 Page({
   data: {
-    
+    songList: {},
   },
   
   onLoad: function () {
+    let self = this;
     // 获取新歌榜
-    
-    console.log(getMusic.musicList)
+    wx.request({
+      url: getMusicUrl.config.newList,
+      data: '',
+      header: {},
+      method: 'GET',
+      dataType: 'json',
+      responseType: 'text',
+      success: function (res) {
+        console.log(res.data);
+        self.setData({
+          songList: res.data
+        })
+      },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
   },
+
+  play: function() {
+    wx.request({
+      url: 'http://tingapi.ting.baidu.com/v1/restserver/ting?method=baidu.ting.song.play&songid=877578',
+      data: '',
+      header: {},
+      method: 'GET',
+      dataType: 'json',
+      responseType: 'text',
+      success: function (res) {
+        console.log(res.data);
+      },
+      fail: function (res) { },
+      complete: function (res) { },
+    })
+  }
 
 })
--- utils/config.js
+++ utils/config.js
@@ -1,30 +1,32 @@
-let method = '';
-let musicAPIurl = 'http://tingapi.ting.baidu.com/v1/restserver/ting';
-
-// method = baidu.ting.billboard.billList & type=1 & size=10 & offset=0
-// 参数：	type = 1 - 新歌榜, 2 - 热歌榜, 11 - 摇滚榜, 12 - 爵士, 16 - 流行, 21 - 欧美金曲榜, 22 - 经典老歌榜, 23 - 情歌对唱榜, 24 - 影视金曲榜, 25 - 网络歌曲榜
-// size = 10 //返回条目数量
-
-//获取新歌榜
-
 (function(module) {
-  method = 'baidu.ting.billboard.billList & type=1 & size=20 & offset=0';
-  let musicList;
-  wx.request({
-    url: musicAPIurl + '?method=' + method,
-    data: '',
-    header: {},
-    method: 'GET',
-    dataType: 'json',
-    responseType: 'text',
-    success: function(res) {
-      
-    },
-    fail: function(res) {},
-    complete: function(res) {},
-  })
+  let musicAPIurl = 'http://tingapi.ting.baidu.com/v1/restserver/ting';
+
+  // 新歌榜单访问接口
+  let newList = musicAPIurl + '?method=baidu.ting.billboard.billList & type=1 & size=20 & offset=0';
+  // 热门榜单访问接口
+  let hotList = musicAPIurl + '?method=baidu.ting.billboard.billList & type=2 & size=20 & offset=0';
+  // 摇滚榜单
+  let yaogun = musicAPIurl + '?method=baidu.ting.billboard.billList & type=11 & size=20 & offset=0';
+  // 爵士榜单
+  let jueshi = musicAPIurl + '?method=baidu.ting.billboard.billList & type=11 & size=20 & offset=0';
+  // 流行榜单
+  let liuxing = musicAPIurl + '?method=baidu.ting.billboard.billList & type=11 & size=20 & offset=0';
+  // 经典老歌榜单
+  let jingdianlaoge = musicAPIurl + '?method=baidu.ting.billboard.billList & type=11 & size=20 & offset=0';
+  // 情歌对唱榜单
+  let qinggeduichang = musicAPIurl + '?method=baidu.ting.billboard.billList & type=11 & size=20 & offset=0';
 
   module.exports = {
-    musicList: res.data
-  }
-}) (module)
+    config: {
+      newList: newList,
+      hotList: hotList,
+      yaogun: yaogun,
+      jueshi: jueshi,
+      liuxing: liuxing,
+      jingdianlaoge: jingdianlaoge,
+      qinggeduichang: qinggeduichang
+    }
+  };
+  
+})(module)
+
